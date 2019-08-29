39e54cda694931b37d948402f241beb0733210ac
dgarcia21 <54485855+dgarcia21@users.noreply.github.com>
2019-08-29 08:22:43 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,14 +1,23 @@
-#include<stdlib.h>
-#include<stdio.h>
+/** 
+ * Author: Dulce Garcia and Taurus Dismuke 
+ * Date: 2019/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+  #include<stdlib.h>
+  #include<stdio.h>
 
 int main(int argc, char **argv) {
 
   printf("Hello World!\n");
 
-  return 0;
-  }
-  int main(int argc, char **argv) {
+ 
    printf("Taurus Dismuke\n");
-   + printf("Computer Science\n");
+   +printf("Computer Science\n");
+ 
+
+  printf("Dulce Garcia\n");
+   +printf("Undeclared\n");
    return 0;
-   }
+  {
