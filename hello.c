bba35e0b8a0ce6d350954f1674e0e0e19ec53b94
T-Debug <54416132+T-Debug@users.noreply.github.com>
2019-08-28 22:37:47 -0500

    Update and rename .gitattributes to hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  
+  return 0;
+  }
+  int main(int argc, char **argv) {
+   printf("Taurus Dismuke\n");
+   + printf("Computer Science\n");
+   return 0;
+   }
+
